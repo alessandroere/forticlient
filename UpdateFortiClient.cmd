eddb294b85079aedca551f2fae3eb5db57f5a420
alessandroere <alessandro.eredia@abstract.it>
2023-11-17 12:21:48 +0100

    Substitute Invoke-WebRequest with curl commando

--- UpdateFortiClient.cmd
+++ UpdateFortiClient.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 echo Downloading FortiClientOnlineSetup...
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Invoke-WebRequest https://github.com/alessandroere/forticlient/raw/main/FortiClientVPNSetup_7.0.10.0538_x64.exe -OutFile C:\FortiClientVPNSetup_7.0.10.0538_x64.exe"
+curl --location --output "C:\FortiClientVPNSetup7010.exe" "https://github.com/alessandroere/forticlient/raw/main/FortiClientVPNSetup_7.0.10.0538_x64.exe"
 
 echo Installing FortiClientVPN...
-C:\FortiClientVPNSetup_7.0.10.0538_x64.exe /quiet /norestart /uninstallfamily
+C:\FortiClientVPNSetup7010.exe /quiet /norestart
 
 echo Deleting FogClient setup file...
-DEL /f C:\FortiClientVPNSetup_7.0.10.0538_x64.exe
+DEL /f C:\FortiClientVPNSetup7010.exe
